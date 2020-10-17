906c38f2bcaad6b0ee3c7d817c01727679a98dd9
jordelucas <jordevalucas@gmail.com>
2020-10-16 23:59:29 -0300

    adiciona landing-page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
+
+import './styles/global.css'
+import './styles/pages/landing.css'
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Canguaretama</strong>
+          <span>Rio Grande do Norte</span>
+        </div>
+
+        <a href="#page-landing" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6" />
+        </a>
+      </div>
     </div>
   );
 }
